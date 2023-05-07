46bad443562b32dac6f5e46bee5b32073bc868d5
phi1goodinc <goodphilinc@gmail.com>
2023-05-07 23:40:54 +0300

    New collection section added.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopScreen from "./TopScreen/TopScreen";
+import NewCollection from "./NewCollection/NewCollection";
 
 const Home = () => {
     return (
         <>
             <TopScreen/>
+            <NewCollection/>
         </>
     );
 };
---
+++ src/components/home/NewCollection/NewCollection.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import photo1 from "../../assets/images/new-collection-photo1.jpg";
+import photo2 from "../../assets/images/new-collection-photo2.jpg";
+import photo3 from "../../assets/images/new-collection-photo3.jpg";
+import arrow from "../../assets/images/arrow.svg";
+import s from './NewCollection.module.css'
+
+const NewCollection = () => {
+    return (
+        <>
+            <div className={`${s.newcollection_container} site_container`}>
+                <h3 className={s.title}>Новая коллекция</h3>
+                <div className={s.offers_container}>
+                    <figure className={s.item}>
+                        <a href="#">
+                            <div className={s.image_container}>
+                                <img className={s.image} src={photo1} alt="Футболка USA"/>
+                                <div className={s.overlay}>
+                                    <a href="#"><img src={arrow} alt="arrow"/></a>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4 className={s.description}>Футболка USA</h4>
+                                <p className={s.price}>$129</p>
+                            </figcaption>
+                        </a>
+
+                    </figure>
+                    <figure className={s.item}>
+                        <a href="#">
+                            <div className={s.image_container}>
+                                <img className={s.image} src={photo2} alt="Купальник Glow"/>
+                                <div className={s.overlay}>
+                                    <a href="#"><img src={arrow} alt="arrow"/></a>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4 className={s.description}>Купальник Glow</h4>
+                                <p className={s.price}>$129</p>
+                            </figcaption>
+                        </a>
+                    </figure>
+                    <figure className={s.item}>
+                        <a href="#">
+                            <div className={s.image_container}>
+                                <img className={s.image} src={photo3} alt="Свитшот Sweet Shot"/>
+                                <div className={s.overlay}>
+                                    <a href="#"><img src={arrow} alt="arrow"/></a>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4 className={s.description}>Свитшот Sweet Shot</h4>
+                                <p className={s.price}>$129</p>
+                            </figcaption>
+                        </a>
+                    </figure>
+                </div>
+                <div className={s.btn_container}>
+                    <button className={s.open_btn}>Открыть магазин</button>
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default NewCollection;
